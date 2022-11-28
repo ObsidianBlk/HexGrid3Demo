3e34685d4c53a83cf76690257587110de88f13f2
Bryan Miller <bmiller1008@gmail.com>
2022-11-28 12:44:43 -0500

    Added the Wedges tool.

--- Scenes/World.gd
+++ Scenes/World.gd
@@ -15,6 +15,8 @@ var _dir_br : Vector2 = Vector2.ZERO
 
 var _operation_mode : String = ""
 var _region_radius : int = 1
+var _wedge_edge : int = 0
+var _wedge_visible : bool = false
 
 var _line_pos : HexCell = HexCell.new()
 var _line_started : bool = false
@@ -24,6 +26,7 @@ var _line_started : bool = false
 # --------------------------------------------------------------------------------------------------
 onready var camera_node : Camera2D = $Camera2D
 onready var hexgrid : HexGrid = $HexGrid
+onready var toolbar : Control = $UI/Toolbar
 
 # --------------------------------------------------------------------------------------------------
 # Override Methods
@@ -36,6 +39,11 @@ func _ready() -> void:
 
 
 func _unhandled_input(event : InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		var fowner = toolbar.get_focus_owner()
+		if fowner:
+			fowner.release_focus()
+	
 	if event.is_action("camera_left", true):
 		_dir_tl.x = event.get_action_strength("camera_left")
 	elif event.is_action("camera_right", true):
@@ -54,6 +62,42 @@ func _unhandled_input(event : InputEvent) -> void:
 				hexgrid.replace_highlight_region("Region", origin.get_region(_region_radius), Color.tomato)
 			elif event.is_action_pressed("interact_alt"):
 				hexgrid.remove_highlight_region("Region")
+		"Wedge":
+			var origin : HexCell = hexgrid.get_origin()
+			if event.is_action_pressed("interact"):
+				_wedge_visible = true
+				hexgrid.replace_highlight_region("Wedge_%s"%[_wedge_edge + 1], origin.get_wedge_region(_wedge_edge, _region_radius), Color.wheat)
+			elif event.is_action_pressed("cycle_up"):
+				hexgrid.remove_highlight_region("Wedge_%s"%[_wedge_edge + 1])
+				_wedge_edge = (_wedge_edge + 1) % 6
+				hexgrid.add_highlight_region("Wedge_%s"%[_wedge_edge + 1], origin.get_wedge_region(_wedge_edge, _region_radius), Color.wheat)
+			elif event.is_action_pressed("cycle_down"):
+				hexgrid.remove_highlight_region("Wedge_%s"%[_wedge_edge + 1])
+				_wedge_edge = 5 if _wedge_edge == 0 else _wedge_edge - 1
+				hexgrid.add_highlight_region("Wedge_%s"%[_wedge_edge + 1], origin.get_wedge_region(_wedge_edge, _region_radius), Color.wheat)
+			elif event.is_action_pressed("interact_alt"):
+				_wedge_visible = false
+				_ClearOp()
+			elif event is InputEventKey:
+				var edge : int = -1
+				match event.scancode:
+					KEY_1:
+						edge = 0
+					KEY_2:
+						edge = 1
+					KEY_3:
+						edge = 2
+					KEY_4:
+						edge = 3
+					KEY_5:
+						edge = 4
+					KEY_6:
+						edge = 5
+				if edge >= 0 and (not _wedge_visible or edge != _wedge_edge):
+					if event.pressed:
+						hexgrid.replace_highlight_region("Wedge_%s"%[edge + 1], origin.get_wedge_region(edge, _region_radius), Color.wheat)
+					else:
+						hexgrid.remove_highlight_region("Wedge_%s"%[edge + 1])
 		"Line":
 			if _line_started and event is InputEventMouseMotion:
 				var mouse_cell : HexCell = HexCell.new(get_global_mouse_position() / hexgrid.cell_size, true, hexgrid.cell_orientation)
@@ -96,6 +140,14 @@ func _ClearOp() -> void:
 	match _operation_mode:
 		"Region":
 			hexgrid.remove_highlight_region("Region")
+		"Wedge":
+			hexgrid.remove_highlight_region("Wedge_1")
+			hexgrid.remove_highlight_region("Wedge_2")
+			hexgrid.remove_highlight_region("Wedge_3")
+			hexgrid.remove_highlight_region("Wedge_4")
+			hexgrid.remove_highlight_region("Wedge_5")
+			hexgrid.remove_highlight_region("Wedge_6")
+			_wedge_visible = false
 		"Line":
 			hexgrid.remove_highlight_region("Line_start")
 			hexgrid.remove_highlight_region("Line")
@@ -111,7 +163,7 @@ func _on_toolbar_operation_requested(req):
 			_ClearOp()
 			_operation_mode = req["op"]
 		match req["op"]:
-			"Region":
+			"Region", "Wedge":
 				if "r" in req:
 					_region_radius = req["r"]
 			"Line":
---
+++ UI/Toolbar/Wedges/Wedges.gd
@@ -0,0 +1,42 @@
+extends Control
+
+# ------------------------------------------------------------------------------
+# Signals
+# ------------------------------------------------------------------------------
+signal operation_requested(req)
+
+# ------------------------------------------------------------------------------
+# Onready Variables
+# ------------------------------------------------------------------------------
+onready var slider : HSlider = $HSlider
+onready var slidervalue_label : Label = $SliderValue
+
+# ------------------------------------------------------------------------------
+# Override Methods
+# ------------------------------------------------------------------------------
+func _ready() -> void:
+	visible = false
+
+# ------------------------------------------------------------------------------
+# Public Methods
+# ------------------------------------------------------------------------------
+func show_if_named(op_name : String) -> void:
+	if op_name == name:
+		emit_signal("operation_requested", {
+			"op":"Wedge",
+			"r": int(slider.value)
+		})
+		visible = true
+	else:
+		visible = false
+
+# ------------------------------------------------------------------------------
+# Handler Methods
+# ------------------------------------------------------------------------------
+func _on_h_slider_value_changed(value : float) -> void:
+	slidervalue_label.text = "[ %s%s ]"%["0" if value < 10 else "", int(value)]
+	emit_signal("operation_requested", {
+		"op":"Wedge",
+		"r": int(value)
+	})
+
